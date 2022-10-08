7ecdd8dd3031597a0021a0d6b281aa814432fc61
kamen <maks.kolesnikov.0202@mail.ru>
2022-10-08 15:34:23 +0300

    cycles. Lesson 2

--- src/Main.java
+++ src/Main.java
@@ -21,12 +21,48 @@ public class Main {
         int population = 12_000_000;
         int birthRate = 17 * population / 1000;
         int deathRate = 8 * population / 1000;
-        for (int c = 1; c <= 10; c++ ) {
+        for (int c = 1; c <= 10; c++) {
             population += birthRate;
             population -= deathRate;
             System.out.println("Год " + c + " численность " + population);
         }
         //Домашнее задание -2 задача первая//
-
+        int initialAmount = 15000;
+        int i1 = 1;
+        while (initialAmount <= 12_000_000 + i1++) {
+            initialAmount *= 1.07;
+            System.out.println("За " + i1 + " Месяцев сумма накоплений равна " + initialAmount + " рублей");
         }
+        //задача вторая//
+        int initialAmount1 = 15000;
+        int i2 = 1;
+        while (initialAmount1 <= 12_000_000 + i2++) {
+            initialAmount1 *= 1.07;
+            if (i2 % 6 == 0)
+                System.out.println("За " + i2 + " Месяцев сумма накоплений равна " + initialAmount1 + " рублей");
+        }
+        //задача третья//
+        int initialAmount2 = 15000;
+        for (int c1 = 1; c1 < 9 * 12; c1 += 6) {
+            initialAmount2 *= 1 + 0.07 * 6;
+            System.out.println("За " + c1 + " месяца сумма накоплений равна " + initialAmount2 + " рублей");
+        }
+        //задача четвертая//
+        int friday = 1;
+        for (int currentFriday = friday; currentFriday <= 31; currentFriday += 7)
+            System.out.println("Сегодня пятница, " + currentFriday + " число. Необходимо подготовить отчет.");
+        //Домашнее задание -3 задача первая//
+        int year = 2022;
+        int beginning = year - 200;
+        int ending = year + 100;
+        for (int n = beginning; n < ending; n++) {
+            if (n % 79 == 0) {
+                System.out.println(n);
+            }
+            //задача вторая //
+            for (int h = 1; h <= 10; h++) {
+                System.out.println("2 * " + h + " = " + 2 * h);
+            }
+        }
+    }
 }
